617fd5283272397270d6ed74cfdf66b0562c1d4c
exersalza (julian j.) <squidodle44@gmail.com>
2023-11-05 18:01:12 +0100

    added git aliases and removed some stuff in the nvim config

--- nvim/custom/configs/lspconfig.lua
+++ nvim/custom/configs/lspconfig.lua
@@ -3,15 +3,15 @@ local capas = require("plugins.configs.lspconfig").capabilities
 local lspconfig = require("lspconfig")
 local util = require "lspconfig/util"
 
-lspconfig.rust_analyzer.setup({
-  on_attach = on_attach,
-  capabilities = capas,
-  filetypes = {"rust"},
-  root_dir = util.root_pattern("Cargo.toml"),
-  settings = {
-
-  }
-})
+-- lspconfig.rust_analyzer.setup({
+--   on_attach = on_attach,
+--   capabilities = capas,
+--   filetypes = {"rust"},
+--   root_dir = util.root_pattern("Cargo.toml"),
+--   settings = {
+--
+--   }
+-- })
 
 --[[
     ["rust_analyzer"] = {
