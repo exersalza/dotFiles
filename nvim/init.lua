c3511e0fba3c307c04457b44251223ee3180d255
exersalza (Julian J.) <exersalza.dev@gmail.com>
2025-07-02 14:20:30 +0200

    custom lua lazy config :speak:

---
+++ nvim/init.lua
@@ -0,0 +1,5 @@
+require("exersalza")
+
+
+vim.cmd[[set rnu shiftwidth=4 tabstop=4 expandtab]]
+vim.cmd[[colorscheme tokyonight]]
---
+++ nvim/lua/exersalza/init.lua
@@ -0,0 +1,8 @@
+require("exersalza.lazy")
+require("exersalza.mappings")
+
+
+require("lualine").setup()
+
+vim.cmd[[set rnu shiftwidth=4 tabstop=4 expandtab clipboard+=unnamedplus]]
+vim.cmd[[colorscheme tokyonight]]
